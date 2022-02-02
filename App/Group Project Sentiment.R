0c3d099f8a2adf413e5e70db50a938e8a72dcd3d
Wajih Arfaoui <50970704+Wajiharfaoui@users.noreply.github.com>
2022-02-02 20:29:49 +0100

    Update Group Project Sentiment.R

--- App/Group Project Sentiment.R
+++ App/Group Project Sentiment.R
@@ -234,10 +234,10 @@ server <- function(input, output){
       theme_minimal()
   })
   
-  output$info <- renderText({
+output$info <- renderText({
     y_str <- function(e) {
       if(is.null(e)) return("NULL\n")
-      paste0("Number of tweets=", round(e$y, 0))
+      paste0("Number of tweets=", round(e$y, 0), "\n Date=", format(as.POSIXct(e$x,origin="1970-01-01"),"%Y-%m-%d"))
     }
     paste0(y_str(input$plot_click))
   })
